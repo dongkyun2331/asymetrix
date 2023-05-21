6fb1a245b523b4eb8c4e0db2962046e7950d5875
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-05-21 22:05:08 +0900

    logo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <img src="./images/logo.png" className="logo-img" alt="logo" />
+    </div>
+  );
+}

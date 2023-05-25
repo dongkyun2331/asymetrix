f6c0ed11fc64e4232ead40b2af5bb08a41e89f3a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-05-25 22:08:18 +0900

    slick

--- src/components/BodyApp.jsx
+++ src/components/BodyApp.jsx
@@ -1,6 +1,9 @@
 import "./BodyApp.css";
 import React, { useState, useEffect } from "react";
 import moment from "moment";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function BodyApp() {
   const [number, setNumber] = useState(0);
@@ -100,6 +103,13 @@ export default function BodyApp() {
     return formattedTime;
   };
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="bodyapp">
       <section className="section">
@@ -177,6 +187,28 @@ export default function BodyApp() {
                 </button>
               </div>
             </div>
+          </div>
+          <div className="ming-content640">
+            <Slider {...settings}>
+              <div>
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+              <div>
+                <h3>5</h3>
+              </div>
+              <div>
+                <h3>6</h3>
+              </div>
+            </Slider>
           </div>
         </div>
         <div className="lavender"></div>
